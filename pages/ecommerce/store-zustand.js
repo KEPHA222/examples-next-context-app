cbf1fd7ee9fdc95da9f264ee2f299d996633baca
kepha222 <kephamotari1@.com>
2022-11-29 12:12:37 +0300

    third commit

---
+++ pages/ecommerce/store-zustand.js
@@ -0,0 +1,17 @@
+import create from "zustand";
+
+const useStore = create((set) => ({
+  user: "",
+  cartCount: 0,
+  login: () => set(() => ({ user: "Kepha" })),
+  logout: () => set(() => ({ user: "" })),
+  addToCart: () => set((state) => ({ cartCount: state.cartCount + 1 })),
+}));
+
+export const useLogin = () => useStore((state) => state.login);
+export const useLogout = () => useStore((state) => state.logout);
+export const useAddToCart = () => useStore((state) => state.addToCart);
+export const useUser = () => useStore((state) => state.user);
+export const useCartCount = () => useStore((state) => state.cartCount);
+
+//export default useStore;
