610a6893e305f8da2d903e0f66b958569ad6a0c5
kepha222 <kephamotari1@.com>
2022-11-29 13:39:11 +0300

    fourth commit

---
+++ pages/ecommerce/store-context.js
@@ -0,0 +1,28 @@
+import { useState, createContext, useContext } from "react";
+
+const useStore = () => {
+  const [user, setUser] = useState("");
+  const [cartCount, setCartCount] = useState(0);
+
+  return {
+    user,
+    cartCount,
+    login: () => setUser("Kepha"),
+    logout: () => setUser(""),
+    addToCart: () => setCartCount(cartCount + 1),
+  };
+};
+
+const StoreContext = createContext(null);
+
+export const StoreContextProvider = ({ children }) => (
+  <StoreContext.Provider value={useStore()}>{children}</StoreContext.Provider>
+);
+
+export const useLogin = () => useContext(StoreContext).login;
+export const useLogout = () => useContext(StoreContext).logout;
+export const useAddToCart = () => useContext(StoreContext).addToCart;
+export const useUser = () => useContext(StoreContext).user;
+export const useCartCount = () => useContext(StoreContext).cartCount;
+
+//export default useStore;
